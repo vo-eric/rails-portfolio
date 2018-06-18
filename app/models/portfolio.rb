b731f3b329e668fefb753a95fac8f83bc5d2188c
Eric Vo <vo.eric@gmail.com>
2018-06-18 16:20:53 -0400

    add concern for image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,17 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.d3
+    where(subtitle: 'D3')
+  end
+
+  scope :ruby_portfolio_items, -> { where(subtitle: 'Ruby') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
