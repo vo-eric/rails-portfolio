bb094c7ba21f1802743569a1622b63baa8e9beb5
Eric Vo <vo.eric@gmail.com>
2018-06-18 16:38:02 -0400

    add nested attributes for portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,9 @@ end
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
