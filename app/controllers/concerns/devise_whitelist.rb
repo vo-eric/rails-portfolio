f96a6476d0a3c4cb2b7fc2ff433c87942bebd631
Eric Vo <vo.eric@gmail.com>
2018-06-19 18:20:42 -0400

    add devise code and session tracker for source link

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,11 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+  included do
+   before_action :configure_parameters, if: :devise_controller?
+  end
+
+  def configure_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+end
