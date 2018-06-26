c9ce405059220d251ee8d958857c0c34113d3a89
Eric Vo <vo.eric@gmail.com>
2018-06-26 19:20:04 -0400

    update file upload forms and refactor placeholder

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "http://via.placeholder.com/#{height}x#{width}"
+  end
+
+  def portfolio_image image, type
+    if image.model.thumb_image? || image.model.main_image?
+      image
+    elsif type == "thumb"
+      image_generator(height: '350', width: '200')
+    elsif type == "main"
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
