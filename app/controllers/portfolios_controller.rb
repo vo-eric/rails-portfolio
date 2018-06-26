1097063bb4852585f5d995d347b8f167f0d9d85f
Eric Vo <vo.eric@gmail.com>
2018-06-26 00:06:17 -0400

    add position update for drag and drop interface for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,6 +6,13 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.by_position
   end
+  
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    render body: nil
+  end
 
   def new
     @portfolio_item = Portfolio.new
