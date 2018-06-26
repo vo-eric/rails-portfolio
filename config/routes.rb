756d46358473ccb0e9a2b724e0a51a1a7a74d859
Eric Vo <vo.eric@gmail.com>
2018-06-26 18:59:59 -0400

    fix edit route for individual portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'portfolio_edit'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
